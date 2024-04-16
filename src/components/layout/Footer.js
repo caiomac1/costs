415faf47a08fe2ec785a94c2087a466d3e61ccc4
caiomac1 <caiomacedo.dev@gmail.com>
2024-04-16 09:42:04 -0300

    criando navbar, footer e pagina home

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,29 @@
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
+
+import styles from './Footer.module.css'
+
+function Footer () {
+    return (
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
+                <li>
+                    <FaGithub />
+                </li>
+
+                <li>
+                    <FaInstagram />
+                </li>
+
+                <li>
+                    <FaLinkedin />
+                </li>
+            </ul>
+
+            <p className={styles.copyright}>
+                <span> Costs </span> &copy; 2024</p>
+
+        </footer>
+    )
+}
+
+export default Footer
