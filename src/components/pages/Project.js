52a7fd9132d5f2655e3995b8d7a45a6537756cfc
caiomac1 <caiomacedo.dev@gmail.com>
2024-04-23 14:35:31 -0300

    Criando área de serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -15,6 +15,8 @@ function Project () {
 
    const [showProjectForm, setShowProjectForm] = useState(false)
 
+   const [showServiceForm, setShowServiceForm] = useState(false)
+
    const [message, setMessage] = useState()
 
    const [type, setType] = useState()
@@ -38,9 +40,12 @@ function Project () {
    }, [id])
 
    function editPost (project) {
+
+    setMessage('')
+
     // budget validation
     if (project.budget < project.cost) {
-        setMessage('O orçamento não pode ser menor que o custo do projeto!')
+        setMessage('O orçamento não pode ser menor que o custo dos serviços!')
         setType('error')
         return false
     }
@@ -67,6 +72,10 @@ function Project () {
 
    function toggleProjectForm () {
     setShowProjectForm(!showProjectForm)
+   }
+
+   function toggleServiceForm () {
+    setShowServiceForm(!showServiceForm)
    }
 
     return (
@@ -98,6 +107,22 @@ function Project () {
                                 </div>
                             )}
                         </div>
+
+                        <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                                {!showServiceForm ? 'Adicionar serviço' :  'Fechar'}
+                            </button>
+
+                            <div className={styles.project_info}>
+                                {showServiceForm && <div>formulário do serviço </div>}
+                            </div>
+                        </div>
+                        <h2>Serviços</h2>
+
+                        <Container customClass="start">
+                            <p>Itens de serviços</p>
+                        </Container>
                     </Container>
                 </div>
             ) : (
